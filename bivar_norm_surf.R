d245a0d64a02cfeb14e9b28da919d5622d271364
nmolanog <nmolanog@gmail.com>
2020-10-31 18:41:48 -0500

    advancing

---
+++ bivar_norm_surf.R
@@ -0,0 +1,39 @@
+rm(list=ls())
+library(animation)
+library(tidyverse)
+library(GA)
+
+my_mean<-c(25,65)
+mycors<-seq(-1,1,by=.25)
+sd_vec<-c(5,7)
+
+i<-3
+temp_cor<-matrix(c(1,mycors[i],
+                   mycors[i],1),
+                 byrow = T,ncol=2)
+V<-sd_vec %*% t(sd_vec) *temp_cor
+
+my_x<-seq(my_mean[1]-3*sd_vec[1], my_mean[1]+3*sd_vec[1], length.out=25)
+my_y<-seq(my_mean[2]-3*sd_vec[2], my_mean[2]+3*sd_vec[2], length.out=25)
+temp_f<-function(a,b){dmvnorm(cbind(a,b), my_mean,V)}
+my_z<-outer(my_x, my_y,temp_f)
+my_theta<-seq(-45,45,length.out = 40)
+my_theta<-c(my_theta,rev(my_theta))
+
+nlevels<-20
+my_zlim <- range(my_z, finite = TRUE)
+my_levels <- pretty(my_zlim, nlevels)
+zz <- (my_z[-1, -1] + my_z[-1, -ncol(my_z)] + my_z[-nrow(my_z), -1] + my_z[-nrow(my_z), 
+                                                         -ncol(my_z)])/4
+cols <- jet.colors(length(my_levels) - 1)
+zzz <- cut(zz, breaks = my_levels, labels = cols)
+
+
+saveGIF({
+  ani.options(interval = 0.2, nmax = length(my_theta))
+  ## use a loop to create images one by one
+  for (i in 1:ani.options('nmax')) {
+    persp(my_x, my_y, my_z, theta = my_theta[i], phi = 45, expand = 0.5,xlab="x",ylab="y",zlab="f(x,y)",col = as.character(zzz))
+    ani.pause()   ## pause for a while ('interval')
+  }
+}, interval = 0.05, movie.name = 'bivar_norm_surf.gif', ani.width = 600, ani.height = 500)
--- testing_ellipses.R
+++ testing_ellipses.R
@@ -53,12 +53,51 @@ ggplot(q.samp, aes(x=x, y=y, z=prob)) +
   annotate("segment", x = c(givenX), xend = c(max(yden_x[,"x"])), 
            y = c(mu_givenX), yend = c(mu_givenX), colour = "red", size=.5, alpha=0.6, arrow=arrow(length = unit(0.03, "npc")))
 
+###############
+rm(list=ls())
+options(max.print=999999)
+library(pacman)
+p_load(tidyverse)
+p_load(mvtnorm)
+p_load(cowplot)
+p_load(gridGraphics)
+p_load(GA)
 
 
+my_mean<-c(25,65)
+mycors<-seq(-1,1,by=.25)
+sd_vec<-c(5,7)
 
-# my_x<-seq(my_mean[1]-3*sd_vec[1], my_mean[1]+3*sd_vec[1], length.out=40)
-# my_y<-seq(my_mean[2]-3*sd_vec[2], my_mean[2]+3*sd_vec[2], length.out=40)
-# temp_f<-function(a,b){dmvnorm(cbind(a,b), my_mean,V)}
-# my_z<-outer(my_x, my_y,temp_f)
-# persp(my_x, my_y, my_z, theta = -10, phi = 45, expand = 0.5, col = "lightblue")
+i<-3
+temp_cor<-matrix(c(1,mycors[i],
+                   mycors[i],1),
+                 byrow = T,ncol=2)
+V<-sd_vec %*% t(sd_vec) *temp_cor
 
+
+my_x<-seq(my_mean[1]-3*sd_vec[1], my_mean[1]+3*sd_vec[1], length.out=20)
+my_y<-seq(my_mean[2]-3*sd_vec[2], my_mean[2]+3*sd_vec[2], length.out=20)
+temp_f<-function(a,b){dmvnorm(cbind(a,b), my_mean,V)}
+my_z<-outer(my_x, my_y,temp_f)
+nlevels<-20
+my_zlim <- range(my_z, finite = TRUE)
+my_levels <- pretty(my_zlim, nlevels)
+zz <- (my_z[-1, -1] + my_z[-1, -ncol(my_z)] + my_z[-nrow(my_z), -1] + my_z[-nrow(my_z), 
+                                                                           -ncol(my_z)])/4
+cols <- jet.colors(length(my_levels) - 1)
+zzz <- cut(zz, breaks = my_levels, labels = cols)
+
+
+persp(my_x, my_y, my_z, theta = -25, phi = 45, expand = 0.5,xlab="x",ylab="y",zlab="f(x,y)",col = as.character(zzz))
+p1 <- recordPlot()  
+
+data.grid <- expand.grid(x = seq(my_mean[1]-3*sd_vec[1], my_mean[1]+3*sd_vec[1], length.out=200),
+                         y = seq(my_mean[2]-3*sd_vec[2], my_mean[2]+3*sd_vec[2], length.out=200))
+q.samp <- cbind(data.grid, prob = dmvnorm(data.grid, mean = my_mean, sigma = V))
+
+
+p2<-ggplot(q.samp, aes(x, y, z = prob)) + 
+  geom_contour(aes(color = ..level..), bins = 11, size = 1) + 
+  scale_color_gradientn(colours = jet.colors(11)) +
+  theme_bw()
+plot_grid(p1, p2)
